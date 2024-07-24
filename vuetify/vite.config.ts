646522e1b5154282772742c874dbe8c124e411eb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-24 18:54:11 +0900

    chore: tweak config

--- vuetify/vite.config.ts
+++ vuetify/vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "vite";
 
 const sassOptions = {
   api: process.env.VITE_SASS_API ?? "modern-compiler",
+
   // https://github.com/sass/dart-sass/issues/2276
   silenceDeprecations: ['mixed-decls'],
+
+  // functions: {
+  //   '__force_no_worker()'() {}
+  // },
 }
 
 export default defineConfig({
